9874603dcc91d4a76f9d27c14d2d1e100a6c2bd4
viktoriyafv <viktoriyafv@users.noreply.github.com>
2024-10-17 09:03:34 +0000

    Deploying to gh-pages from @ htmlacademy/drink2go-check@8468281bfd7103459279c15ff0113ae2601c312e 🚀

---
+++ 4/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav"),e=document.querySelector(".main-nav__toggle");n.classList.remove("main-nav--nojs");e.onclick=function(){n.classList.toggle("main-nav--opened")};
